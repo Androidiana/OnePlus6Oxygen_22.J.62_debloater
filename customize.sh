539a59d4bc2ae5f4378c2141579e2d585be9aab8
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2022-01-31 23:01:24 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,29 +15,27 @@
 
 keytest() 
 {
-	ui_print " ########################################### "
-	ui_print "#                                           #"
-    ui_print "# Vol Key Test                              #"
-	ui_print "# - - - - - - - - - - - - - - - - - - - - - #"
-    ui_print "#    Press Vol Up:                          #"
-	ui_print "#                                           #"
-    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 0
-    return 1
+	ui_print " "
+    ui_print " Vol Key Test                              "
+	ui_print " - - - - - - - - - - - - - - - - - - - - - "
+    ui_print "   Press Vol Up ( + )                      "
+	ui_print "                                           "
+    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 1
+    return 0
 }
 
 # Ask user for MDD mode
 # ---------------------
 choose() 
 {
-	ui_print " ########################################### "
-	ui_print "#                                           #"
-	ui_print "# Select debloating mode                    #"
-	ui_print "# - - - - - - - - - - - - - - - - - - - - - #"
-	ui_print "#                                           #"
-	ui_print "# ( + ) = FULL: OxygenOs as /e              #"
-	ui_print "# ( - ) = LITE: debloating, gapps preserved #"
-	ui_print "# - - - - - - - - - - - - - - - - - - - - - #"
-	ui_print "#                                           #"
+	ui_print " "
+	ui_print " Select debloating mode                    "
+	ui_print " - - - - - - - - - - - - - - - - - - - - - "
+	ui_print "                                           "
+	ui_print " ( + ) = FULL: OxygenOs as /e              "
+	ui_print " ( - ) = LITE: debloating, gapps preserved "
+	ui_print " - - - - - - - - - - - - - - - - - - - - - "
+	ui_print "                                           "
 
     #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
     while (true); do
@@ -65,14 +63,13 @@ fi
 # Remove directories
 # ----------------------------------------------------
 if "$FUNC"; then
-	ui_print "# plus detected - going FULL mode           #"
+	ui_print " plus detected - going FULL mode           "
 else
     rm -r "$MODPATH/system/app/com.aurora.store" "$MODPATH/system/framework" "$MODPATH/system/priv-app/CalendarProvider" "$MODPATH/system/priv-app/DiracAudioControlService" "$MODPATH/system/priv-app/DiracManager" "$MODPATH/system/priv-app/OnePlusCamera" "$MODPATH/system/priv-app/OnePlusCameraService" "$MODPATH/system/product/app/GoogleContactsSyncAdapter" "$MODPATH/system/product/app/WebViewGoogle" "$MODPATH/system/product/priv-app/GmsCore" "$MODPATH/system/product/priv-app/Phonesky" "$MODPATH/system/system_ext/app/DynamicDDSService" "$MODPATH/system/system_ext/app/OPFilemanager" "$MODPATH/system/system_ext/priv-app/Dialer" "$MODPATH/system/system_ext/priv-app/OPContacts" "$MODPATH/system/system_ext/priv-app/OPMms"
-	ui_print "# minus detected - going LITE mode          #"
+	ui_print " minus detected - going LITE mode          "
 fi
 
-ui_print "# - - - - - - - - - - - - - - - - - - - - - #"
-ui_print "#                                           #"
-ui_print "# enjoy!                                    #"
-ui_print "#                                           #"
-ui_print " ########################################### "
+ui_print " - - - - - - - - - - - - - - - - - - - - - "
+ui_print "                                           "
+ui_print " enjoy!                                    "
+ui_print "                                           "
